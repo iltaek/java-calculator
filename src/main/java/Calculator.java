988dd1a5d2679187e5b4044a9b73f8331b466f4a
joon36 <kwjoon7@gmail.com>
2019-12-12 01:08:02 +0900

    코드스타일 변경

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,49 +1,57 @@
 import com.google.common.base.Preconditions;
-import lombok.AllArgsConstructor;
-
-import java.util.*;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.Queue;
 import java.util.function.BinaryOperator;
 import java.util.stream.Collectors;
+import lombok.AllArgsConstructor;
 
 public class Calculator {
 
-    @AllArgsConstructor
-    enum Operator {
-        ADD("+", Double::sum),
-        SUBTRACT("-", (a, b) -> a - b),
-        MULTIPLY("*", (a, b) -> a * b),
-        DIVIDE("/", (a, b) -> a / b);
+  public double calculate(String expression) {
+    final List<String> arguments = Arrays.asList(expression.split(" "));
+    checkValidation(arguments);
+    final Queue<Operator> operators = arguments
+        .stream()
+        .map(Operator::findByName)
+        .filter(Optional::isPresent)
+        .map(Optional::get)
+        .collect(Collectors.toCollection(LinkedList::new));
+    return arguments.stream()
+        .filter(o -> !Operator.findByName(o).isPresent())
+        .map(Double::parseDouble)
+        .reduce((a, b) -> Objects.requireNonNull(operators.poll()).method.apply(a, b))
+        .orElseThrow(RuntimeException::new);
+  }
 
-        String name;
-        BinaryOperator<Double> method;
-
-        static Optional<Operator> findByName(String name) {
-            return Arrays.stream(Operator.values())
-                    .filter(v -> v.name.equals(name))
-                    .findFirst();
-        }
+  private void checkValidation(List<String> arguments) {
+    Preconditions.checkArgument(arguments.size() % 2 == 1);
+    int i;
+    for (i = 0;
+        i < arguments.size() && Operator.findByName(arguments.get(i)).isPresent() == (i % 2 == 1);
+        i++) {
+      ;
     }
+    Preconditions.checkArgument(arguments.size() == i);
+  }
 
-    public double calculate(String expression) {
-        final List<String> arguments = Arrays.asList(expression.split(" "));
-        checkValidation(arguments);
-        final Queue<Operator> operators = arguments
-                .stream()
-                .map(Operator::findByName)
-                .filter(Optional::isPresent)
-                .map(Optional::get)
-                .collect(Collectors.toCollection(LinkedList::new));
-        return arguments.stream()
-                .filter(o -> !Operator.findByName(o).isPresent())
-                .map(Double::parseDouble)
-                .reduce((a, b) -> Objects.requireNonNull(operators.poll()).method.apply(a, b))
-                .orElseThrow(RuntimeException::new);
-    }
+  @AllArgsConstructor
+  enum Operator {
+    ADD("+", Double::sum),
+    SUBTRACT("-", (a, b) -> a - b),
+    MULTIPLY("*", (a, b) -> a * b),
+    DIVIDE("/", (a, b) -> a / b);
 
-    private void checkValidation(List<String> arguments) {
-        Preconditions.checkArgument(arguments.size()%2 == 1);
-        int i;
-        for (i = 0; i < arguments.size() && Operator.findByName(arguments.get(i)).isPresent() == (i%2 == 1); i++);
-        Preconditions.checkArgument(arguments.size() == i);
+    String name;
+    BinaryOperator<Double> method;
+
+    static Optional<Operator> findByName(String name) {
+      return Arrays.stream(Operator.values())
+          .filter(v -> v.name.equals(name))
+          .findFirst();
     }
+  }
 }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,55 +1,55 @@
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 public class CalculatorTest {
 
-    @Test
-    @DisplayName("계산기1 기능 테스트")
-    void calculate1Test() {
-        Calculator calculator = new Calculator();
-        assertThat(calculator.calculate("1 + 2")).isEqualTo(3.0);
-        assertThat(calculator.calculate("1")).isEqualTo(1.0);
-        assertThat(calculator.calculate("1 - 2 + 3 * 4 / 5")).isEqualTo(1.6);
-        assertThrows(IllegalArgumentException.class, () -> {
-            calculator.calculate("1 + 1 1 + 1");
-        });
-        assertThrows(IllegalArgumentException.class, () -> {
-            calculator.calculate("1 + - 1 + 1");
-        });
-        assertThrows(IllegalArgumentException.class, () -> {
-            calculator.calculate("+ 1 1 + 1");
-        });
-        assertThrows(IllegalArgumentException.class, () -> {
-            calculator.calculate("1 + 1 +");
-        });
-        assertThrows(IllegalArgumentException.class, () -> {
-            calculator.calculate("+");
-        });
-    }
+  @Test
+  @DisplayName("계산기1 기능 테스트")
+  void calculate1Test() {
+    Calculator calculator = new Calculator();
+    assertThat(calculator.calculate("1 + 2")).isEqualTo(3.0);
+    assertThat(calculator.calculate("1")).isEqualTo(1.0);
+    assertThat(calculator.calculate("1 - 2 + 3 * 4 / 5")).isEqualTo(1.6);
+    assertThrows(IllegalArgumentException.class, () -> {
+      calculator.calculate("1 + 1 1 + 1");
+    });
+    assertThrows(IllegalArgumentException.class, () -> {
+      calculator.calculate("1 + - 1 + 1");
+    });
+    assertThrows(IllegalArgumentException.class, () -> {
+      calculator.calculate("+ 1 1 + 1");
+    });
+    assertThrows(IllegalArgumentException.class, () -> {
+      calculator.calculate("1 + 1 +");
+    });
+    assertThrows(IllegalArgumentException.class, () -> {
+      calculator.calculate("+");
+    });
+  }
 
-    @Test
-    @DisplayName("계산기2 기능 테스트")
-    void calculate2Test() {
-        assertThat(Calculator2.calculate("1 + 2")).isEqualTo(3.0);
-        assertThat(Calculator2.calculate("1")).isEqualTo(1.0);
-        assertThat(Calculator2.calculate("1 - 2 + 3 * 4 / 5")).isEqualTo(1.6);
-        assertThrows(IllegalArgumentException.class, () -> {
-            Calculator2.calculate("1 + 1 1 + 1");
-        });
-        assertThrows(IllegalArgumentException.class, () -> {
-            Calculator2.calculate("1 + - 1 + 1");
-        });
-        assertThrows(IllegalArgumentException.class, () -> {
-            Calculator2.calculate("+ 1 1 + 1");
-        });
-        assertThrows(IllegalArgumentException.class, () -> {
-            Calculator2.calculate("1 + 1 +");
-        });
-        assertThrows(IllegalArgumentException.class, () -> {
-            Calculator2.calculate("+");
-        });
-    }
+  @Test
+  @DisplayName("계산기2 기능 테스트")
+  void calculate2Test() {
+    assertThat(Calculator2.calculate("1 + 2")).isEqualTo(3.0);
+    assertThat(Calculator2.calculate("1")).isEqualTo(1.0);
+    assertThat(Calculator2.calculate("1 - 2 + 3 * 4 / 5")).isEqualTo(1.6);
+    assertThrows(IllegalArgumentException.class, () -> {
+      Calculator2.calculate("1 + 1 1 + 1");
+    });
+    assertThrows(IllegalArgumentException.class, () -> {
+      Calculator2.calculate("1 + - 1 + 1");
+    });
+    assertThrows(IllegalArgumentException.class, () -> {
+      Calculator2.calculate("+ 1 1 + 1");
+    });
+    assertThrows(IllegalArgumentException.class, () -> {
+      Calculator2.calculate("1 + 1 +");
+    });
+    assertThrows(IllegalArgumentException.class, () -> {
+      Calculator2.calculate("+");
+    });
+  }
 }
