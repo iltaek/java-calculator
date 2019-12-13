aeadcb018bfd2d9be477530b9c318b5631d01dcd
iltaek <iltaekkwon@gmail.com>
2019-12-13 10:06:01 +0900

    feat($all): 계산기 및 테스트 구현

---
+++ src/main/java/calc/InputChecker.java
@@ -0,0 +1,38 @@
+package calc;
+
+import java.util.Objects;
+import java.util.regex.Pattern;
+
+public class InputChecker {
+
+    private static final String NULL_INPUT = "아무 것도 입력하지 않았습니다.";
+    private static final String ILLEGAL_FORMULA = "입력한 식을 다시 한번 확인해주세요.";
+    private static final String INPUT_DELIMETER = " ";
+    private static final String INPUT_PATTERN = "^[\\d]+( [+|\\-|*|/] [\\d]+)*";
+    private static final Pattern input_pattern = Pattern.compile(INPUT_PATTERN);
+    private String[] formula;
+
+    public InputChecker(String input) {
+        this.formula = checkInput(input);
+    }
+
+    public String[] getFormula() {
+        return formula;
+    }
+
+    private String[] checkInput(String input) {
+        if (Objects.isNull(input)) {
+            throw new IllegalArgumentException(NULL_INPUT);
+        }
+
+        if (!isValidFormula(input)) {
+            throw new IllegalArgumentException(ILLEGAL_FORMULA);
+        }
+
+        return input.split(INPUT_DELIMETER);
+    }
+
+    private boolean isValidFormula(String input) {
+        return input_pattern.matcher(input).matches();
+    }
+}
---
+++ src/main/java/calc/operations/Operators.java
@@ -0,0 +1,37 @@
+package calc.operations;
+
+import java.util.HashMap;
+
+public enum Operators {
+    PLUS("+", new Addition()),
+    MINUS("-", new Subtraction()),
+    MULTIPLY("*", new Multiplication()),
+    DIVIDE("/", new Division());
+
+    private String operator;
+    private Operation operation;
+    private static final HashMap<String, Operators> OP_LABEL = new HashMap<>();;
+
+    static {
+        for (Operators op : values())
+            OP_LABEL.put(op.toString(), op);
+    }
+
+    Operators(String operator, Operation operation) {
+        this.operator = operator;
+        this.operation = operation;
+    }
+
+    public static Operators getOperation(String op) {
+        return OP_LABEL.get(op);
+    }
+
+    public double calculate(double op1, double op2) {
+        return operation.calculate(op1, op2);
+    }
+
+    @Override
+    public String toString() {
+        return operator;
+    }
+}
