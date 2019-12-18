02974895f586126f5fe6cc94ed8531809528cc38
joon36 <kwjoon7@gmail.com>
2019-12-18 23:29:33 +0900

    operator 탐색로직을 메서드로 분리

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -10,17 +10,24 @@ public class Calculator {
 
     public double calculate(String expression) {
         final List<String> arguments = Arrays.asList(expression.split(" "));
+
         Verifier.checkValidation(arguments);
-        final Queue<Operator> operators = arguments
-            .stream()
-            .map(Operator::findByName)
-            .filter(Optional::isPresent)
-            .map(Optional::get)
-            .collect(Collectors.toCollection(LinkedList::new));
+
+        final Queue<Operator> operators = findOperators(arguments);
+
         return arguments.stream()
             .filter(o -> !Operator.findByName(o).isPresent())
             .map(Double::parseDouble)
             .reduce((a, b) -> Objects.requireNonNull(operators.poll()).method.apply(a, b))
             .orElseThrow(() -> new IllegalArgumentException("계산에 실패했습니다."));
+    }
+
+    private Queue<Operator> findOperators(List<String> arguments) {
+        return arguments
+            .stream()
+            .map(Operator::findByName)
+            .filter(Optional::isPresent)
+            .map(Optional::get)
+            .collect(Collectors.toCollection(LinkedList::new));
     }
 }
